ca520b6a328dc15a0d5aca25c63962bfa4e9c60c
Pham Quang Thanh <petrpham11@gmail.com>
2025-06-21 18:04:33 +0200

    Implement SceneManager for scene switching

---
+++ EndGameScene.cs
@@ -0,0 +1,18 @@
+﻿using Nez;
+using Microsoft.Xna.Framework;
+
+namespace bluewarp
+{
+    internal class EndGameScene : BaseScene
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SetDesignResolution(GameConstants.GameWidth, GameConstants.GameHeight, SceneResolutionPolicy.ShowAllPixelPerfect);
+            Screen.SetSize(GameConstants.ScaledGameWidth, GameConstants.ScaledGameHeight);
+            ClearColor = Color.Blue;
+
+        }
+    }
+}
---
+++ SceneManager.cs
@@ -0,0 +1,17 @@
+﻿using Nez;
+
+namespace bluewarp
+{
+    public static class SceneManager
+    {
+        public static void LoadGameScene()
+        {
+            Core.StartSceneTransition(new FadeTransition( () => new RunGameScene()));
+        }
+
+        public static void LoadGameOver()
+        {
+            Core.StartSceneTransition(new FadeTransition( () => new EndGameScene()));
+        }
+    }
+}
